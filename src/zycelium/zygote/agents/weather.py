b93c628767ab73c914ace88017b217b4010d7684
Harshad Sharma <harshad@sharma.io>
2023-04-06 05:41:16 +0530

    Add weather agent

---
+++ src/zycelium/zygote/agents/weather.py
@@ -0,0 +1,23 @@
+"""
+Weather updates from OpenWeatherMap.
+"""
+from sunnyside import Sunnyside
+from zycelium.zygote.agent import Agent
+
+agent = Agent(name="weather", debug=True)
+
+agent.config = {
+    "api_key": "",
+    "city": "",
+    "units": "metric",
+}
+
+
+@agent.on_startup(delay=1)
+@agent.on_interval(seconds=60)
+async def weather():
+    """Update weather periodically."""
+    sunnyside = Sunnyside(api_key=agent.config["api_key"], units=agent.config["units"])
+    weather_api = sunnyside.current_weather()
+    weather_data = weather_api.get_current_weather_by_city_name(city_name=agent.config["city"])
+    await agent.emit(event="weather/current", data=weather_data)
