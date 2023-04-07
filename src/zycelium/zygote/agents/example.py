c4bcd01ca10db8a249d29675fa328b6f40f2c958
Harshad Sharma <harshad@sharma.io>
2023-04-07 10:23:28 +0530

    Skip delay at startup

--- src/zycelium/zygote/agents/example.py
+++ src/zycelium/zygote/agents/example.py
@@ -6,7 +6,7 @@ from zycelium.zygote.agent import Agent
 agent = Agent(name="example", debug=True)
 
 
-@agent.on_startup(delay=1)
+@agent.on_startup()
 @agent.on_interval(minutes=1)
 async def example():
     """Example function."""
