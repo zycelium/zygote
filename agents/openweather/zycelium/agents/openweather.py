e79a03b6973c2c62b7faf18b3195d4e08b4460c6
Harshad Sharma <harshad@sharma.io>
2023-04-07 15:12:15 +0530

    Validate config

--- agents/openweather/zycelium/agents/openweather.py
+++ agents/openweather/zycelium/agents/openweather.py
@@ -13,13 +13,35 @@ agent.config = {
 }
 
 
+async def validate_config():
+    """Validate config."""
+    if not agent.config["api_key"]:
+        await agent.emit(
+            "openweather/error", {"message": "OpenWeather API key not provided."}
+        )
+        return False
+    if not agent.config["city"]:
+        await agent.emit("openweather/error", {"message": "City not provided."})
+        return False
+    if not agent.config["units"]:
+        await agent.emit("openweather/error", {"message": "Units not provided."})
+        return False
+    if agent.config["units"] not in ["metric", "imperial"]:
+        await agent.emit(
+            "openweather/error", {"message": "Units must be metric or imperial."}
+        )
+        return False
+
+
 @agent.on_startup(delay=1)
 @agent.on_interval(minutes=10)
 async def weather():
     """Update weather periodically."""
+    if not await validate_config():
+        return
     sunnyside = Sunnyside(api_key=agent.config["api_key"], units=agent.config["units"])
     weather_api = sunnyside.current_weather()
     weather_data = weather_api.get_current_weather_by_city_name(
         city_name=agent.config["city"]
     )
-    await agent.emit("weather/current", weather_data)
+    await agent.emit("openweather/current", weather_data)
