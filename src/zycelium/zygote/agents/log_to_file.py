940e5ea679912f15727c0b3023327e7e68ff62f8
Harshad Sharma <harshad@sharma.io>
2023-04-07 09:38:57 +0530

    Add log to file agent.

---
+++ src/zycelium/zygote/agents/log_to_file.py
@@ -0,0 +1,18 @@
+"""
+Log to file agent.
+"""
+import json
+from zycelium.zygote.agent import Agent
+
+agent = Agent(name="log_to_file", debug=True)
+
+agent.config = {
+    "log_file": "zygote_events.log",
+}
+
+
+@agent.on("*")
+async def log_to_file(name, data):
+    """Log events to file."""
+    with open(agent.config["log_file"], "a", encoding="utf-8") as file:
+        file.write(json.dumps({"kind": "event", "name": name, "data": data}) + "\n")
