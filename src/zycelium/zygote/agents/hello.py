5c1a5a04ad08bc37210d9e862c587fb7b2c3ce00
Harshad Sharma <harshad@sharma.io>
2023-04-07 10:30:56 +0530

    Rename example -> hello agent

--- src/zycelium/zygote/agents/hello.py
+++ src/zycelium/zygote/agents/hello.py
@@ -3,11 +3,11 @@ Example agent.
 """
 from zycelium.zygote.agent import Agent
 
-agent = Agent(name="example", debug=True)
+agent = Agent(name="hello", debug=True)
 
 
 @agent.on_startup()
 @agent.on_interval(minutes=1)
 async def example():
     """Example function."""
-    await agent.emit("example", {"message": "Hello, world!"})
+    await agent.emit("hello", {"message": "Hello, world!"})
