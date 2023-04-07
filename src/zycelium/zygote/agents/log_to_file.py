d6dd4b0668764cba77434a78853bff88e3c93283
Harshad Sharma <harshad@sharma.io>
2023-04-07 10:23:17 +0530

    Rename events to frames

--- src/zycelium/zygote/agents/log_to_file.py
+++ src/zycelium/zygote/agents/log_to_file.py
@@ -7,12 +7,12 @@ from zycelium.zygote.agent import Agent
 agent = Agent(name="log_to_file", debug=True)
 
 agent.config = {
-    "log_file": "zygote_events.log",
+    "log_file": "zygote_frames.log",
 }
 
 
 @agent.on("*")
 async def log_to_file(_kind, frame):
-    """Log events to file."""
+    """Log frames to file."""
     with open(agent.config["log_file"], "a", encoding="utf-8") as file:
         file.write(json.dumps(frame) + "\n")
