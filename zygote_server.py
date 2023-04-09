21f0d89d5f346355e749f40c92cca781c5234790
Harshad Sharma <harshad@sharma.io>
2023-04-09 21:09:14 +0530

    Add command to edit agent config

--- zygote_server.py
+++ zygote_server.py
@@ -1,16 +1,37 @@
 import asyncio
+from pathlib import Path
+
 import click
 from zycelium.zygote.server import Server
 
 
-@click.command()
+
+@click.group()
+def main():
+    """Zygote Server"""
+
+
+@main.command()
 @click.option("--host", default="localhost", help="Host")
 @click.option("--port", default=3965, help="Port")
 @click.option("--debug", default=False, help="Debug")
-def main(host, port, debug):
-    """Zygote App"""
+def serve(host, port, debug):
+    """Run Zygote Server"""
     server = Server(name="zygote", debug=debug)
     asyncio.run(server.start(host=host, port=port))
+
+
+@main.command()
+@click.argument("agent")
+def config(agent):
+    """Edit config file for agent in user's EDITOR."""
+    config_path = click.get_app_dir("zygote")
+    config_file = Path(config_path).joinpath(f"{agent}.json")
+    if config_file.exists():
+        click.edit(filename=str(config_file))
+    else:
+        click.echo(f"Config file for agent {agent} does not exist.")
+
 
 
 if __name__ == "__main__":
