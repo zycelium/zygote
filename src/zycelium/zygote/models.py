7683c3103b58119e8f4bf8d80ab496c441655b25
Harshad Sharma <harshad@sharma.io>
2023-04-10 22:33:37 +0530

    Add Space model

--- src/zycelium/zygote/models.py
+++ src/zycelium/zygote/models.py
@@ -48,3 +48,33 @@ PydanticFrameIn = pydantic_model_creator(
     Frame, name="FrameIn", exclude_readonly=True, exclude=("uuid", "time")
 )
 PydanticFrameList = pydantic_queryset_creator(Frame, name="FrameList")
+
+
+class Space(Model):
+    """Space model"""
+
+    uuid = fields.UUIDField(pk=True, index=True)
+    name = fields.CharField(max_length=64, index=True, null=False)
+    data = fields.JSONField(default={})
+    meta = fields.JSONField(default={})
+    created_at = fields.DatetimeField(auto_now_add=True, index=True)
+    updated_at = fields.DatetimeField(auto_now=True, index=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    class Meta:
+        """Meta class"""
+
+        table = "space"
+        ordering = ["name"]
+
+
+PydanticSpace = pydantic_model_creator(Space, name="Space")
+PydanticSpaceIn = pydantic_model_creator(
+    Space,
+    name="SpaceIn",
+    exclude_readonly=True,
+    exclude=("uuid", "created_at", "updated_at"),
+)
+PydanticSpaceList = pydantic_queryset_creator(Space, name="SpaceList")
