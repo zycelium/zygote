db7e15175433df9b446a8aa33f966971f99144cb
Harshad Sharma <harshad@sharma.io>
2023-04-10 07:37:52 +0530

    Add comments

--- src/zycelium/zygote/signals.py
+++ src/zycelium/zygote/signals.py
@@ -3,10 +3,11 @@ Async signals for zygote.
 """
 from async_signals.dispatcher import Signal
 
-
+# Process signals
 process_started = Signal()
 process_stopped = Signal()
 
+# Supervisor signals
 supervisor_started = Signal()
 supervisor_stopped = Signal()
 supervisor_cancelled = Signal()
