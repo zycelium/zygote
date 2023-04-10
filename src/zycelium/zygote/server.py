d9b785eda2817379062beef30220550a7cc607e2
Harshad Sharma <harshad@sharma.io>
2023-04-10 08:51:56 +0530

    Remove testing code

--- src/zycelium/zygote/server.py
+++ src/zycelium/zygote/server.py
@@ -15,32 +15,10 @@ sup = Supervisor()
 log = get_logger("zygote.server")
 
 
-def dummy_process(name) -> None:
-    """
-    A dummy process.
-    """
-    import time
-    import random
-
-    log_ = get_logger(f"zygote.dummy.{name}")
-    try:
-        while True:
-            if random.randint(0, 10) == 2:
-                raise RuntimeError("Boom!")
-            log_.info("tick")
-            time.sleep(3)
-    except KeyboardInterrupt:
-        pass
-
-
 @app.before_serving
 async def before_serving():
     """Startup hook."""
     log.info("Starting server")
-    await sup.add_process("dummy1", dummy_process, "dummy1")
-    await sup.add_process("dummy2", dummy_process, "dummy2")
-    await sup.add_process("dummy3", dummy_process, "dummy3")
-    await sup.add_process("dummy4", dummy_process, "dummy4")
     await sup.start()
 
 
@@ -55,9 +33,3 @@ async def after_serving():
 async def index():
     """Index route."""
     return "Hello, World!"
-
-
-@app.route("/processes")
-async def processes():
-    """List processes."""
-    return {"processes": list(sup.processes)}
