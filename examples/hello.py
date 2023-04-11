8d14d0ba34bf42e2ab107222712d115105cb2e0c
Harshad Sharma <harshad@sharma.io>
2023-04-11 21:59:07 +0530

    Add example: hello

---
+++ examples/hello.py
@@ -0,0 +1,24 @@
+from zycelium.zygote.agent import Agent
+
+
+agent = Agent('hello')
+
+
+@agent.on('connect')
+async def on_connect() -> None:
+    """On connect."""
+    print('Connected to server')
+    await agent.emit('hello', {'name': agent.name})
+
+
+@agent.on('hello')
+async def on_hello(data: dict) -> None:
+    """On hello."""
+    print(f'Hello {data["name"]}')
+    await agent.disconnect()
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(agent.run("https://localhost:3965/"))
