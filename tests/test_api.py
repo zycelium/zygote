971d6eac47da69cc3b41909ef0136db64e85b8fa
Harshad Sharma <harshad@sharma.io>
2023-04-11 17:20:59 +0530

    Use pytest fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,211 +1,154 @@
 """Test API."""
-from zycelium.zygote.api import ZygoteAPI
 
 
-async def test_create_space():
+async def test_create_space(api):
     """Test create space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     assert space["name"] == "test"
-    await api.stop()
 
 
-async def test_create_space_not_unique_fail():
+async def test_create_space_not_unique_fail(api):
     """Test create space fail."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     assert space["name"] == "test"
     space = await api.create_space("test")
     assert space["success"] is False
-    await api.stop()
 
 
-async def test_get_space():
+async def test_get_space(api):
     """Test get space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     space = await api.get_space(space["uuid"])
     assert space["name"] == "test"
-    await api.stop()
 
 
-async def test_update_space():
+async def test_update_space(api):
     """Test update space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     space = await api.update_space(space["uuid"], name="test2")
     assert space["name"] == "test2"
-    await api.stop()
 
 
-async def test_get_spaces():
+async def test_get_spaces(api):
     """Test get spaces."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     await api.create_space("test")
     await api.create_space("test2")
     spaces = await api.get_spaces()
     assert len(spaces["spaces"]) == 2
-    await api.stop()
 
 
-async def test_create_agent():
+async def test_create_agent(api):
     """Test create agent."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     agent = await api.create_agent("test")
     assert agent["name"] == "test"
-    await api.stop()
 
 
-async def test_create_agent_not_unique_fail():
+async def test_create_agent_not_unique_fail(api):
     """Test create agent fail."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     agent = await api.create_agent("test")
     assert agent["name"] == "test"
     agent = await api.create_agent("test")
     assert agent["success"] is False
-    await api.stop()
 
 
-async def test_get_agent():
+async def test_get_agent(api):
     """Test get agent."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     agent = await api.create_agent("test")
     agent = await api.get_agent(agent["uuid"])
     assert agent["name"] == "test"
-    await api.stop()
 
 
-async def test_update_agent():
+async def test_update_agent(api):
     """Test update agent."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     agent = await api.create_agent("test")
     agent = await api.update_agent(agent["uuid"], name="test2")
     assert agent["name"] == "test2"
-    await api.stop()
 
 
-async def test_get_agents():
+async def test_get_agents(api):
     """Test get agents."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     await api.create_agent("test")
     await api.create_agent("test2")
     agents = await api.get_agents()
     assert len(agents["agents"]) == 2
-    await api.stop()
 
 
-async def test_delete_agent():
+async def test_delete_agent(api):
     """Test delete agent."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     agent = await api.create_agent("test")
     agent = await api.delete_agent(agent["uuid"])
     assert agent["success"] is True
-    await api.stop()
 
 
-async def test_join_space():
+async def test_join_space(api):
     """Test join space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     agent_joined = await api.get_agent(agent["uuid"])
     assert agent_joined["spaces"][0]["uuid"] == space["uuid"]
-    await api.stop()
 
 
-async def test_leave_space():
+async def test_leave_space(api):
     """Test leave space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     await api.leave_space(space["uuid"], agent["uuid"])
     agent_joined = await api.get_agent(agent["uuid"])
     assert space["uuid"] not in agent_joined["spaces"]
-    await api.stop()
 
 
-async def test_get_joined_spaces():
+async def test_get_joined_spaces(api):
     """Test get joined spaces."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     spaces = await api.get_joined_spaces(agent["uuid"])
     assert len(spaces["spaces"]) == 1
     assert spaces["spaces"][0]["uuid"] == space["uuid"]
-    await api.stop()
 
 
-async def test_get_unjoined_spaces():
+async def test_get_unjoined_spaces(api):
     """Test get unjoined spaces."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     spaces = await api.get_unjoined_spaces(agent["uuid"])
     assert len(spaces["spaces"]) == 0
-    await api.stop()
 
 
-async def test_get_joined_agents():
+async def test_get_joined_agents(api):
     """Test get joined agents."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     agents = await api.get_joined_agents(space["uuid"])
     assert len(agents["agents"]) == 1
     assert agents["agents"][0]["uuid"] == agent["uuid"]
-    await api.stop()
 
 
-async def test_get_unjoined_agents():
+async def test_get_unjoined_agents(api):
     """Test get unjoined agents."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     await api.join_space(space["uuid"], agent["uuid"])
     agents = await api.get_unjoined_agents(space["uuid"])
     assert len(agents["agents"]) == 0
-    await api.stop()
 
 
-async def test_create_frame():
+async def test_create_frame(api):
     """Test create frame."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     frame = await api.create_frame(
         kind="event", name="test", agent_uuid=agent["uuid"], space_uuids=[space["uuid"]]
     )
     assert frame["name"] == "test"
-    await api.stop()
 
 
-async def test_get_frame():
+async def test_get_frame(api):
     """Test get frame."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     frame = await api.create_frame(
@@ -215,13 +158,10 @@ async def test_get_frame():
     assert frame["name"] == "test"
     assert frame["spaces"][0]["uuid"] == space["uuid"]
     assert frame["agent"]["uuid"] == agent["uuid"]
-    await api.stop()
 
 
-async def test_get_frames_for_agent():
+async def test_get_frames_for_agent(api):
     """Test get frames for agent."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     frame = await api.create_frame(
@@ -230,13 +170,10 @@ async def test_get_frames_for_agent():
     frames = await api.get_frames_for_agent(agent["uuid"])
     assert len(frames["frames"]) == 1
     assert frames["frames"][0]["uuid"] == frame["uuid"]
-    await api.stop()
 
 
-async def test_get_frames_for_space():
+async def test_get_frames_for_space(api):
     """Test get frames for space."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     frame = await api.create_frame(
@@ -245,13 +182,10 @@ async def test_get_frames_for_space():
     frames = await api.get_frames_for_space(space["uuid"])
     assert len(frames["frames"]) == 1
     assert frames["frames"][0]["uuid"] == frame["uuid"]
-    await api.stop()
 
 
-async def test_delete_frame():
+async def test_delete_frame(api):
     """Test delete frame."""
-    api = ZygoteAPI()
-    await api.start("sqlite://:memory:")
     space = await api.create_space("test")
     agent = await api.create_agent("test")
     frame = await api.create_frame(
@@ -260,4 +194,3 @@ async def test_delete_frame():
     await api.delete_frame(frame["uuid"])
     frames = await api.get_frames_for_agent(agent["uuid"])
     assert len(frames["frames"]) == 0
-    await api.stop()
