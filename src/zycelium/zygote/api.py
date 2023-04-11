5311cba7868467e6ac36d887db991a16683f0119
Harshad Sharma <harshad@sharma.io>
2023-04-11 13:37:39 +0530

    Pydantic with Tortoise is a pain.

--- src/zycelium/zygote/api.py
+++ src/zycelium/zygote/api.py
@@ -4,22 +4,18 @@ Zygote API.
 from typing import Optional
 
 from tortoise import Tortoise
+from tortoise.query_utils import Prefetch
+from tortoise.contrib.pydantic.creator import (
+    pydantic_model_creator,
+    pydantic_queryset_creator,
+)
 
 from zycelium.zygote.logging import get_logger
 from zycelium.zygote.models import (
     init_db,
     Frame,
-    PydanticFrame,
-    PydanticFrameIn,
-    PydanticFrameList,
     Space,
-    PydanticSpace,
-    PydanticSpaceIn,
-    PydanticSpaceList,
     Agent,
-    PydanticAgent,
-    PydanticAgentIn,
-    PydanticAgentList,
 )
 
 
@@ -50,7 +46,13 @@ class ZygoteAPI:
         meta = meta or {}
         try:
             space_obj = await Space.create(name=name, data=data, meta=meta)
-            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+            space_dict = {
+                "uuid": space_obj.uuid,
+                "name": space_obj.name,
+                "data": space_obj.data,
+                "meta": space_obj.meta,
+            }
+            return space_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to create space: %s", name)
             return {"success": False}
@@ -60,7 +62,13 @@ class ZygoteAPI:
         self.logger.info("Getting space")
         try:
             space_obj = await Space.get(uuid=space_uuid)
-            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+            space_dict = {
+                "uuid": space_obj.uuid,
+                "name": space_obj.name,
+                "data": space_obj.data,
+                "meta": space_obj.meta,
+            }
+            return space_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to get space: %s", space_uuid)
             return {"success": False}
@@ -69,8 +77,17 @@ class ZygoteAPI:
         """Get spaces."""
         self.logger.info("Getting spaces")
         try:
-            spaces = await PydanticSpaceList.from_queryset(Space.all())
-            return {"spaces": spaces.dict()["__root__"]}
+            spaces = await Space.all()
+            spaces_list = []
+            for space in spaces:
+                space_dict = {
+                    "uuid": space.uuid,
+                    "name": space.name,
+                    "data": space.data,
+                    "meta": space.meta,
+                }
+                spaces_list.append(space_dict)
+            return {"spaces": spaces_list}
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to get spaces")
             return {"success": False}
@@ -92,7 +109,13 @@ class ZygoteAPI:
             space_obj.data = data  # type: ignore
             space_obj.meta = meta  # type: ignore
             await space_obj.save()
-            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+            space_dict = {
+                "uuid": space_obj.uuid,
+                "name": space_obj.name,
+                "data": space_obj.data,
+                "meta": space_obj.meta,
+            }
+            return space_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to update space: %s", space_uuid)
             return {"success": False}
@@ -117,7 +140,13 @@ class ZygoteAPI:
         meta = meta or {}
         try:
             agent_obj = await Agent.create(name=name, data=data, meta=meta)
-            return (await PydanticAgent.from_tortoise_orm(agent_obj)).dict()
+            agent_dict = {
+                "uuid": agent_obj.uuid,
+                "name": agent_obj.name,
+                "data": agent_obj.data,
+                "meta": agent_obj.meta,
+            }
+            return agent_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to create agent: %s", name)
             return {"success": False}
@@ -126,18 +155,36 @@ class ZygoteAPI:
         """Get agent."""
         self.logger.info("Getting agent")
         try:
-            agent_obj = await Agent.get(uuid=agent_uuid)
-            return (await PydanticAgent.from_tortoise_orm(agent_obj)).dict()
+            agent_obj = await Agent.get(uuid=agent_uuid).prefetch_related(
+                "spaces", Prefetch("spaces", queryset=Space.all())
+            )
+            agent_dict = {
+                "uuid": agent_obj.uuid,
+                "name": agent_obj.name,
+                "data": agent_obj.data,
+                "meta": agent_obj.meta,
+                "spaces": [space.uuid for space in await agent_obj.spaces],
+            }
+            return agent_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to get agent: %s", agent_uuid)
             return {"success": False}
-    
+
     async def get_agents(self) -> dict:
         """Get agents."""
         self.logger.info("Getting agents")
         try:
-            agents = await PydanticAgentList.from_queryset(Agent.all())
-            return {"agents": agents.dict()["__root__"]}
+            agents = await Agent.all()
+            agents_list = []
+            for agent in agents:
+                agent_dict = {
+                    "uuid": agent.uuid,
+                    "name": agent.name,
+                    "data": agent.data,
+                    "meta": agent.meta,
+                }
+                agents_list.append(agent_dict)
+            return {"agents": agents_list}
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to get agents")
             return {"success": False}
@@ -159,7 +206,13 @@ class ZygoteAPI:
             agent_obj.data = data  # type: ignore
             agent_obj.meta = meta  # type: ignore
             await agent_obj.save()
-            return (await PydanticAgent.from_tortoise_orm(agent_obj)).dict()
+            agent_dict = {
+                "uuid": agent_obj.uuid,
+                "name": agent_obj.name,
+                "data": agent_obj.data,
+                "meta": agent_obj.meta,
+            }
+            return agent_dict
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to update agent: %s", agent_uuid)
             return {"success": False}
@@ -173,4 +226,16 @@ class ZygoteAPI:
             return {"success": True}
         except Exception:  # pylint: disable=broad-except
             self.logger.error("Failed to delete agent: %s", agent_uuid)
+            return {"success": False}
+
+    async def join_space(self, space_uuid: int, agent_uuid: int) -> dict:
+        """Join space."""
+        self.logger.info("Joining space")
+        try:
+            space_obj = await Space.get(uuid=space_uuid)
+            agent_obj = await Agent.get(uuid=agent_uuid)
+            await space_obj.agents.add(agent_obj)  # type: ignore
+            return {"success": True}
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to join space: %s", space_uuid)
             return {"success": False}
