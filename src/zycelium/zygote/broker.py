574b4014e5c827dbe571eaa20c431fdb2009ee0d
Harshad Sharma <harshad@sharma.io>
2023-04-12 00:01:15 +0530

    Authenticate agent connections

--- src/zycelium/zygote/broker.py
+++ src/zycelium/zygote/broker.py
@@ -3,13 +3,23 @@ Frame broker.
 """
 import socketio
 
+from zycelium.zygote.logging import get_logger
+
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+log = get_logger("zygote.broker")
 
 
 @sio.on("connect")
 async def connect(sid, _environ, auth: dict):
     """On connected."""
-    print("connect ", sid, auth)
+    from zycelium.zygote.server import api  # pylint: disable=import-outside-toplevel
+
+    log.info("Agent connected: %s", sid)
+    agent = await api.get_agent_by_token(auth["token"])
+    if agent == {"success": False}:
+        log.error("Invalid token: %s", auth["token"])
+        return False
+    log.info("Agent authenticated: %s", agent["name"])
 
 
 @sio.on("disconnect")
