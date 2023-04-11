024edf1304650729684400441148777efbfb2dac
Harshad Sharma <harshad@sharma.io>
2023-04-12 00:02:16 +0530

    Send token auth; format

--- examples/hello.py
+++ examples/hello.py
@@ -4,24 +4,29 @@ Hello world example.
 from zycelium.zygote.agent import Agent
 
 
-agent = Agent('hello')
+agent = Agent("example")
 
 
-@agent.on('connect')
+@agent.on("connect")
 async def on_connect() -> None:
     """On connect."""
-    print('Connected to server')
-    await agent.emit('hello', {'name': agent.name})
+    print("Connected to server")
+    await agent.emit("hello", {"name": agent.name})
 
 
-@agent.on('hello')
+@agent.on("hello")
 async def on_hello(data: dict) -> None:
     """On hello."""
     print(f'Hello {data["name"]}')
     await agent.disconnect()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(agent.run("https://localhost:3965/", auth={'token': '1234'}))
+    asyncio.run(
+        agent.run(
+            "https://localhost:3965/",
+            auth={"token": ""},
+        )
+    )
