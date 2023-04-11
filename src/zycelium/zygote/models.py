0976c222d9a1387f4fe09940a2804882ec4b2efe
Harshad Sharma <harshad@sharma.io>
2023-04-11 23:59:31 +0530

    Add AuthToken model

--- src/zycelium/zygote/models.py
+++ src/zycelium/zygote/models.py
@@ -84,3 +84,24 @@ class Agent(Model):
 
         table = "agent"
         ordering = ["name"]
+
+
+
+class AuthToken(Model):
+    """AuthToken model"""
+
+    uuid = fields.UUIDField(pk=True, index=True)
+    token = fields.CharField(max_length=64, index=True, null=False, unique=True)
+    agent = fields.ForeignKeyField("models.Agent", related_name="tokens", null=True)
+    revoked = fields.BooleanField(default=False)
+    created_at = fields.DatetimeField(auto_now_add=True, index=True)
+    updated_at = fields.DatetimeField(auto_now=True, index=True)
+
+    def __str__(self) -> str:
+        return f"{self.token}"
+
+    class Meta:
+        """Meta class"""
+
+        table = "auth_token"
+        ordering = ["-created_at"]
