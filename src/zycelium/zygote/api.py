8973d9ed6612c9ce664093dfce63a9d7eb92f791
Harshad Sharma <harshad@sharma.io>
2023-04-11 12:57:12 +0530

    Add Space methods to API

--- src/zycelium/zygote/api.py
+++ src/zycelium/zygote/api.py
@@ -1,7 +1,10 @@
 """
 Zygote API.
 """
+from typing import Optional
+
 from tortoise import Tortoise
+
 from zycelium.zygote.logging import get_logger
 from zycelium.zygote.models import (
     init_db,
@@ -35,3 +38,63 @@ class ZygoteAPI:
         """Stop."""
         self.logger.info("Stopping")
         await Tortoise.close_connections()
+
+    async def create_space(self, name: str, data: Optional[dict] = None, meta: Optional[dict] = None):
+        """Create space."""
+        self.logger.info("Creating space")
+        data = data or {}
+        meta = meta or {}
+        try:
+            space_obj = await Space.create(name=name, data=data, meta=meta)
+            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to create space: %s", name)
+            return {"success": False}
+    
+    async def get_space(self, space_uuid: int):
+        """Get space."""
+        self.logger.info("Getting space")
+        try:
+            space_obj = await Space.get(uuid=space_uuid)
+            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to get space: %s", space_uuid)
+            return {"success": False}
+    
+    async def get_spaces(self):
+        """Get spaces."""
+        self.logger.info("Getting spaces")
+        try:
+            spaces = await PydanticSpaceList.from_queryset(Space.all())
+            return spaces.dict()["__root__"]
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to get spaces")
+            return {"success": False}
+
+    
+    async def update_space(self, space_uuid: int, name: str, data: Optional[dict] = None, meta: Optional[dict] = None):
+        """Update space."""
+        self.logger.info("Updating space")
+        data = data or {}
+        meta = meta or {}
+        try:
+            space_obj = await Space.get(uuid=space_uuid)
+            space_obj.name = name  # type: ignore
+            space_obj.data = data  # type: ignore
+            space_obj.meta = meta  # type: ignore
+            await space_obj.save()
+            return (await PydanticSpace.from_tortoise_orm(space_obj)).dict()
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to update space: %s", space_uuid)
+            return {"success": False}
+    
+    async def delete_space(self, space_uuid: int):
+        """Delete space."""
+        self.logger.info("Deleting space")
+        try:
+            space_obj = await Space.get(id=space_uuid)
+            await space_obj.delete()
+            return {"success": True}
+        except Exception:  # pylint: disable=broad-except
+            self.logger.error("Failed to delete space: %s", space_uuid)
+            return {"success": False}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,28 +1,55 @@
 """Test API."""
 import pytest
-from tortoise import Tortoise
 
-from zycelium.zygote.models import init_db
-from zycelium.zygote.server import app as zygote_app
+from zycelium.zygote.api import ZygoteAPI
 
 
-@pytest.fixture(scope="function", autouse=True)
-async def app():
-    """Quart app"""
-    await init_db("sqlite://:memory:")
-    yield zygote_app
-    await Tortoise.close_connections()
+async def test_create_space():
+    """Test create space."""
+    api = ZygoteAPI()
+    await api.start("sqlite://:memory:")
+    space = await api.create_space("test")
+    assert space["name"] == "test"
+    await api.stop()
 
 
-async def test_frame_post(app):  # pylint: disable=redefined-outer-name
-    """Test frame post."""
-    client = app.test_client()
-    data = {"kind": "event", "name": "test", "data": {"foo": "bar"}}
-    response = await client.post("/api/v1/frames", json=data)
-    assert response.status_code == 200
-    json = await response.json
-    assert json["kind"] == "event"
-    assert json["name"] == "test"
-    assert json["data"] == {"foo": "bar"}
-    assert json["meta"] == {}
-    assert json["time"] is not None
+async def test_create_space_not_unique_fail():
+    """Test create space fail."""
+    api = ZygoteAPI()
+    await api.start("sqlite://:memory:")
+    space = await api.create_space("test")
+    assert space["name"] == "test"
+    space = await api.create_space("test")
+    assert space["success"] is False
+    await api.stop()
+
+
+async def test_get_space():
+    """Test get space."""
+    api = ZygoteAPI()
+    await api.start("sqlite://:memory:")
+    space = await api.create_space("test")
+    space = await api.get_space(space["uuid"])
+    assert space["name"] == "test"
+    await api.stop()
+
+
+async def test_update_space():
+    """Test update space."""
+    api = ZygoteAPI()
+    await api.start("sqlite://:memory:")
+    space = await api.create_space("test")
+    space = await api.update_space(space["uuid"], name="test2")
+    assert space["name"] == "test2"
+    await api.stop()
+
+
+async def test_get_spaces():
+    """Test get spaces."""
+    api = ZygoteAPI()
+    await api.start("sqlite://:memory:")
+    await api.create_space("test")
+    await api.create_space("test2")
+    spaces = await api.get_spaces()
+    assert len(spaces) == 2
+    await api.stop()
