946a9a4a748cc738fc88ce77c00f6b5c9c795452
Harshad Sharma <harshad@sharma.io>
2023-04-11 23:11:34 +0530

    Format

--- src/zycelium/zygote/agent.py
+++ src/zycelium/zygote/agent.py
@@ -6,23 +6,24 @@ import socketio
 
 class Agent:
     """Agent."""
+
     def __init__(self, name: str) -> None:
         self.name = name
         self.sio = socketio.AsyncClient(ssl_verify=False)
         self.on = self.sio.on  # pylint: disable=invalid-name
-    
+
     async def connect(self, url: str, auth: dict) -> None:
         """Connect to server."""
         await self.sio.connect(url, auth=auth)
-    
+
     async def disconnect(self) -> None:
         """Disconnect from server."""
         await self.sio.disconnect()
-    
+
     async def emit(self, event: str, data: dict) -> None:
         """Emit event."""
         await self.sio.emit(event, data)
-    
+
     async def run(self, url: str, auth: dict) -> None:
         """Run agent."""
         await self.connect(url, auth=auth)
