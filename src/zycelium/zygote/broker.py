cccbce388b26b6a529d8214370f5f9abf67538af
Harshad Sharma <harshad@sharma.io>
2023-04-12 08:40:01 +0530

    Use spaces to join roooms, use dict to map sid to agent

--- src/zycelium/zygote/broker.py
+++ src/zycelium/zygote/broker.py
@@ -4,32 +4,55 @@ Frame broker.
 import socketio
 
 from zycelium.zygote.logging import get_logger
+from zycelium.zygote.api import api
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 log = get_logger("zygote.broker")
+SID_AGENT = {}
 
 
 @sio.on("connect")
 async def connect(sid, _environ, auth: dict):
     """On connected."""
-    from zycelium.zygote.server import api  # pylint: disable=import-outside-toplevel
 
     log.info("Agent connected: %s", sid)
+
+    # Authenticate agent
     agent = await api.get_agent_by_token(auth["token"])
     if agent == {"success": False}:
         log.error("Invalid token: %s", auth["token"])
         return False
     log.info("Agent authenticated: %s", agent["name"])
 
+    # Store agent
+    SID_AGENT[sid] = agent
+
+    # Add agent to spaces
+    for space in agent["spaces"]:
+        sio.enter_room(sid, space["uuid"])
+        log.info("Agent %s joined space %s", agent["name"], space["name"])
+
+    # Send command: identity
+    await sio.emit(
+        "command",
+        {"name": "idenity", "data": {"name": agent["name"], "spaces": agent["spaces"]}},
+        room=sid,
+    )
+
 
 @sio.on("disconnect")
 def disconnect(sid):
     """On disconnected."""
-    print("disconnect ", sid)
+    agent = SID_AGENT.pop(sid, None)
+    log.info("Agent disconnected: %s", agent["name"] if agent else sid)
 
 
 @sio.on("*")
 async def frame(event, sid, data):
     """On frame."""
-    print("frame ", event, sid, data)
+    # Store frames in database
+    # broadcast to all joined spaces if space not specified
+    agent = SID_AGENT[sid]
+
+    print("frame ", event, agent["name"], data)
     await sio.emit(event, data)
