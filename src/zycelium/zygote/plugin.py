3913c4930bd53a2df5201f7e0aaee863be4176e9
Harshad Sharma <harshad@sharma.io>
2023-04-12 23:18:03 +0530

    Add start_agent method

--- src/zycelium/zygote/plugin.py
+++ src/zycelium/zygote/plugin.py
@@ -1,6 +1,7 @@
 """
 Plug-in agents discovery.
 """
+import asyncio
 import importlib
 import pkgutil
 from typing import Iterable
@@ -29,3 +30,15 @@ def discover_agents() -> Iterable[str]:
     for _, name, _ in _iter_namespace(namespace):
         log.info("Found installed agent: %s", name)
         yield name
+
+
+def start_agent(agent_name: str, url: str, auth: dict) -> None:
+    """Start an agent."""
+    log.info("Starting agent %s", agent_name)
+    agent_module = importlib.import_module(agent_name)
+    try:
+        log.info("Starting agent %s", agent_name)
+        asyncio.run(agent_module.agent.run(url=url, auth=auth))
+    except Exception as exc:  # pylint: disable=broad-except
+        log.exception(exc)
+    log.info("Started agent %s", agent_name)
