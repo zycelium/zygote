6c2ddfd33a9182e9fb032cee965e4b479bceb1d8
Harshad Sharma <harshad@sharma.io>
2023-04-12 10:15:34 +0530

    Process identity command

--- src/zycelium/zygote/agent.py
+++ src/zycelium/zygote/agent.py
@@ -9,8 +9,10 @@ class Agent:
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.spaces = {}
         self.sio = socketio.AsyncClient(ssl_verify=False)
         self.on = self.sio.on  # pylint: disable=invalid-name
+        self.sio.on("command", self._on_command)
 
     async def connect(self, url: str, auth: dict) -> None:
         """Connect to server."""
@@ -23,6 +25,18 @@ class Agent:
     async def emit(self, event: str, data: dict) -> None:
         """Emit event."""
         await self.sio.emit(event, data)
+
+    async def _on_command(self, data: dict) -> None:
+        """On command."""
+        command = data["name"]
+        if command == "identity":
+            agent_name = data["data"]["name"]
+            spaces = data["data"]["spaces"]
+            self.name = agent_name
+            self.spaces = {space["name"]: space for space in spaces}
+            print(f"Agent {agent_name} joined spaces: {', '.join(self.spaces.keys())}")
+        else:
+            print(f"Unknown command: {command}")
 
     async def run(self, url: str, auth: dict) -> None:
         """Run agent."""
