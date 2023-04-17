0b29e1edfd68628b12a45e52cfca6bd64b8b8d48
Harshad Sharma <harshad@sharma.io>
2023-04-18 01:44:40 +0530

    Fixes #12

--- src/zycelium/zygote/utils.py
+++ src/zycelium/zygote/utils.py
@@ -11,6 +11,7 @@ def secret_key(path: Path) -> str:
     Get a secret key from a file.
     """
     if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
         key = secrets.token_urlsafe(32)
         path.write_text(key)
     return path.read_text().strip()
