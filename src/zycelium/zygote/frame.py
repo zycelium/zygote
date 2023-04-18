b7b647a98ddd4f2b445f41fbdc634096bf5a1682
Harshad Sharma <harshad@sharma.io>
2023-04-18 20:25:37 +0530

    Add Frame for #3

---
+++ src/zycelium/zygote/frame.py
@@ -0,0 +1,29 @@
+"""
+Frame.
+"""
+from dataclasses import dataclass, field, asdict
+
+
+@dataclass
+class Frame:
+    """Frame."""
+
+    name: str
+    kind: str
+    data: dict = field(default_factory=dict)
+    meta: dict = field(default_factory=dict)
+
+    @classmethod
+    def from_dict(cls, frame_dict: dict) -> "Frame":
+        """Create frame from dict."""
+        return cls(**frame_dict)
+
+    def to_dict(self):
+        """Return dict representation of frame."""
+        return asdict(self)
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def __repr__(self):
+        return str(self)
