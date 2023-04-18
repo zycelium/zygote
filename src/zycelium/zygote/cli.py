b35ae0f8398ea8903f68f3cb4cbeba950eb53b48
Harshad Sharma <harshad@sharma.io>
2023-04-19 02:38:35 +0530

    Add peer management cli

--- src/zycelium/zygote/cli.py
+++ src/zycelium/zygote/cli.py
@@ -1,6 +1,7 @@
 """
 Zygote CLI
 """
+import asyncio
 from pathlib import Path
 
 import click
@@ -77,3 +78,63 @@ def config_reset(obj, yes):
         )
     else:
         click.echo("Config was not modified.")
+
+
+@main.group()
+def peer():
+    """Manage peers."""
+
+
+@peer.command("add")
+@click.option("--name", prompt=True)
+@click.option("--url", prompt=True)
+def peer_add(name, url):
+    """Add peer."""
+    click.echo(f"Add peer: {name} {url}")
+
+    async def _add():
+        await zygote.models.start_database(zygote.config.database_url)
+        await zygote.models.Peer.create(name=name, base_url=url)
+        await zygote.models.stop_database()
+
+    asyncio.run(_add())
+
+
+@peer.command("list")
+def peer_list():
+    """List peers."""
+
+    async def _list():
+        await zygote.models.start_database(zygote.config.database_url)
+        peers = await zygote.models.Peer.all()
+        if not peers:
+            click.echo("No peers found.")
+            await zygote.models.stop_database()
+            return
+
+        click.echo("Peers:")
+        for peer in peers:
+            click.echo(f"{peer.name} {peer.base_url}")
+        await zygote.models.stop_database()
+
+    asyncio.run(_list())
+
+
+@peer.command("remove")
+@click.option("--name", prompt=True)
+def peer_remove(name):
+    """Remove peer."""
+    click.echo(f"Remove peer: {name}")
+
+    async def _remove():
+        await zygote.models.start_database(zygote.config.database_url)
+        try:
+            peer = await zygote.models.Peer.get(name=name)
+            await peer.delete()
+        except zygote.models.DoesNotExist:
+            click.echo(f"Peer not found: {name}")
+            return
+        finally:
+            await zygote.models.stop_database()
+
+    asyncio.run(_remove())
