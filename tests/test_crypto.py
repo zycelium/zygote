601a0e5c90106032f28b0132f53d285e7f71ea0e
Harshad Sharma <harshad@sharma.io>
2023-04-18 20:50:16 +0530

    Update tests for crypto

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -3,22 +3,25 @@ Test cryptographic utilities.
 """
 from pathlib import Path
 from OpenSSL import crypto
-from zycelium.zygote.crypto import generate_self_signed_certificate
+from zycelium.zygote.crypto import ensure_tls_certificate_chain
 
 
-async def test_generate_self_signed_certificate(tmp_path: Path) -> None:
-    """
-    Test generating a self-signed certificate.
-    """
-    cert_path = tmp_path / "cert.pem"
-    key_path = tmp_path / "key.pem"
-    generate_self_signed_certificate(cert_path, key_path, 1)
-    assert cert_path.exists()
-    assert key_path.exists()
-    with open(cert_path, "rt", encoding="utf-8") as cert_file:
-        cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_file.read())  # type: ignore
-    assert cert.get_subject().CN == "localhost"
-    with open(key_path, "rt", encoding="utf-8") as key_file:
-        key = crypto.load_privatekey(crypto.FILETYPE_PEM, key_file.read())
-    assert key.bits() == 4096
-    assert key.type() == crypto.TYPE_RSA
+def test_ensure_tls_certificate_chain(tmp_path: Path):
+    domain_name = "test.example.com"
+    valid_days = 365
+    app_dir = tmp_path
+
+    paths = ensure_tls_certificate_chain(domain_name, app_dir, valid_days)
+
+    assert paths.ca.exists()
+    assert paths.key.exists()
+    assert paths.cert.exists()
+
+    with open(paths.key, "rb") as key_file:
+        key_cert = crypto.load_certificate(crypto.FILETYPE_PEM, key_file.read())
+        assert key_cert.get_subject().CN == domain_name
+
+    with open(paths.cert, "rb") as cert_file:
+        cert_cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_file.read())
+        assert cert_cert.get_subject().CN == domain_name
+
