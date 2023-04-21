54d6eb73a3273124f9740d8cfc2a2c8fe90a2045
Harshad Sharma <harshad@sharma.io>
2023-04-21 20:57:01 +0530

    Use server identities for cors

--- src/zycelium/zygote/instance/app.py
+++ src/zycelium/zygote/instance/app.py
@@ -4,9 +4,11 @@ Zygote Instance App.
 from quart import Quart
 from quart_cors import cors
 
+from zycelium.zygote import config
+
 
 app = Quart(__name__)
-app = cors(app, allow_origin="*")
+app = cors(app, allow_origin=config.server_identities)
 
 
 @app.route("/")
