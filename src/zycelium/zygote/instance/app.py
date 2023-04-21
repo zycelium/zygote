911f832041975880dad9dca2e8bfdadb31d8b3fb
Harshad Sharma <harshad@sharma.io>
2023-04-22 00:08:14 +0530

    Start LocalDNS on quart app start

--- src/zycelium/zygote/instance/app.py
+++ src/zycelium/zygote/instance/app.py
@@ -5,10 +5,27 @@ from quart import Quart
 from quart_cors import cors
 
 from zycelium.zygote import config
-
+from zycelium.zygote.discovery import LocalDNS
 
 app = Quart(__name__)
 app = cors(app, allow_origin=config.server_identities)
+
+
+@app.before_serving
+async def before_serving():
+    """Before serving."""
+    app.local_dns = LocalDNS()
+    await app.local_dns.start(
+        domain="zygote",
+        host=config.http_host,
+        port=config.http_port,
+    )
+
+
+@app.after_serving
+async def after_serving():
+    """After serving."""
+    await app.local_dns.stop()
 
 
 @app.route("/")
